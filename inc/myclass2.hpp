a6bc3efc4bedc099bc18880b2225d91d8132083d
jstrombe19 <jaredstromberg@gmail.com>
2024-10-02 18:45:44 -0600

    added second class (duplicate as poc) to yml and modified yml2cpp.py to ingest multiple classes and iteratively generate their header files

--- inc/myclass2.hpp
+++ inc/myclass2.hpp
@@ -1,10 +1,10 @@
 // WARNING::Auto-generated file; do not modify this file! If change is necessary, update the corresponding yml and re-generate!
 #pragma once
 
-#ifndef _MYCLASS_H_
-#define _MYCLASS_H_
+#ifndef _MYCLASS2_H_
+#define _MYCLASS2_H_
 
-class MyClass {
+class MyClass2 {
 public:
      int x;
      double y;
--- tools/yml2cpp.py
+++ tools/yml2cpp.py
@@ -5,48 +5,68 @@ with open('../inc/nebula.yml', 'r') as file:
   data = yaml.safe_load(file)
 
 
-def generate_hpp(data):
+def generate_hpp(class_data):
   hpp_content = ""
-
+  hpp_content += "// WARNING::Auto-generated file; do not modify this file! If change is necessary, update the corresponding yml and re-generate!\n"
   hpp_content += "#pragma once\n\n"
 
 
-  for class_data in data['classes']:
-    class_name = class_data['name']
-    upper_class_name = class_name.upper()
-    
-    hpp_content += "#ifndef _" + upper_class_name + "_H_\n"
-    hpp_content += "#define _" + upper_class_name + "_H_\n\n"
-    hpp_content += f"class {class_name} {{\n"
-    
-    # Add members
-    hpp_content += "public:\n"
-    for member in class_data['members']:
-      hpp_content += f"     {member['type']} {member['name']};\n"
+  # for class_data in data['classes']:
+  class_name = class_data['name']
+  upper_class_name = class_name.upper()
+  
+  hpp_content += "#ifndef _" + upper_class_name + "_H_\n"
+  hpp_content += "#define _" + upper_class_name + "_H_\n\n"
+  hpp_content += f"class {class_name} {{\n"
+  
+  # Add members
+  hpp_content += "public:\n"
+  for member in class_data['members']:
+    hpp_content += f"     {member['type']} {member['name']};\n"
 
-    # Add methods
-    for method in class_data['methods']:
-      hpp_content += f"     {method['return_type']} {method['name']}("
-      params = ", ".join([f"{param['type']} {param['name']}" for param in method['parameters']])  
-      hpp_content += f"{params});\n"
+  # Add methods
+  for method in class_data['methods']:
+    hpp_content += f"     {method['return_type']} {method['name']}("
+    params = ", ".join([f"{param['type']} {param['name']}" for param in method['parameters']])  
+    hpp_content += f"{params});\n"
 
-    hpp_content += "};\n\n"
+  hpp_content += "};\n\n"
 
-    hpp_content += "#endif"
+  hpp_content += "#endif"
   return hpp_content
 
-hpp_content = generate_hpp(data)
+def write_hpp(data):
+  for classdata in data['classes']:
+    hpp_content = generate_hpp(classdata)
 
-with open('../inc/myclass.hpp', 'w') as file:
-  file.write(hpp_content)
+    classname = '../inc/' + classdata['name'].lower() + '.hpp'
+    with open(classname, 'w') as file:
+      file.write(hpp_content)
+
+
+write_hpp(data)
+
+
+# hpp_content = generate_hpp(data)
+
+# with open('../inc/myclass.hpp', 'w') as file:
+#   file.write(hpp_content)
 
 
 def generate_cpp(data):
   cpp_content = ""
+  cpp_content += "#include \"../inc/" + data['classes']['name'] + "\"\n"
+
+  for class_data in data['classes']:
+    for method in class_data['methods']:
+      cpp_content += f"     {method['return_type']} {method['name']}("
+      params = ", ".join([f"{param['type']} {param['name']}" for param in method['parameters']])
+      cpp_content += f"{params});\n"
+    
 
   return cpp_content
 
-cpp_content = generate_cpp(data)
+# cpp_content = generate_cpp(data)
 
-with open('../src/myclass.cpp', 'w') as file:
-  file.write(cpp_content)
+# with open('../src/myclass.cpp', 'w') as file:
+#   file.write(cpp_content)
